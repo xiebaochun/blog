3a0e194d238ff031b13a53a0178ccc37f809aad8
xbc <463449055@qq.com>
2016-08-16 13:41:08 +0800

    update

--- push.js
+++ push.js
@@ -4,8 +4,8 @@ if(exec('git commit -m "update"').code !==0){
 	echo(`Error: Git commit source failed`);
     exit(1);
 }
-if (exec(`git push origin source`).code !== 0) {
+if (exec(`git push origin source-zh`).code !== 0) {
 	echo(`Error: Git push source failed`);
 	exit(1);
 }
-echo(`------------ source updated`);
+echo(`------------ source-zh updated`);
