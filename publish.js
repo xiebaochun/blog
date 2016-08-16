92e06ff833c860aae21ad6e0632ea15d33354447
xbc <463449055@qq.com>
2016-08-16 16:53:58 +0800

    update

--- publish.js
+++ publish.js
@@ -6,12 +6,18 @@ cd('build');
 exec('git add .');
 if(exec('git commit -m "update"').code !==0){
 	echo(`Error: Git commit gh-pages failed`);
-    exit(1);
+    out();
 }
 if (exec(`git push origin gh-pages`).code !== 0) {
 	echo(`Error: Git push gh-pages failed`);
-	exit(1);
+	out();
 }
 echo(`------------ gh-pages updated`);
+out();
 
-cd('..');
+function out(argument) {
+	// body...
+	cd('..');
+	exec('gulp build');
+	exit(1);
+}
