d9841f9f772344ecd4764e3674f606ada261e3b3
xbc <463449055@qq.com>
2016-12-26 11:29:14 +0800

    update

--- publish.js
+++ publish.js
@@ -2,15 +2,26 @@ var build = require('./build');
 build(false);
 
 require(`shelljs/global`);
+
 cd('build');
-exec('git add .');
-if(exec('git commit -m "update"').code !==0){
-	echo(`Error: Git commit gh-pages failed`);
-    out();
-}
-if (exec(`git push origin gh-pages`).code !== 0) {
-	echo(`Error: Git push gh-pages failed`);
-	out();
+
+publish();
+
+function publish(){
+	exec('git add .');
+	if(exec('git commit -m "update"').code !== 0){
+		echo(`Error: Git commit gh-pages failed`);
+	    out();
+	}
+	if (exec(`git push origin gh-pages`).code !== 0) {
+		echo(`Error: Git push gh-pages failed`);
+		exec(`git init`);
+		exec(`git remote add origin https://xiebaochun:dandan520@github.com/xiebaochun/blog.git`);
+		
+		publish();
+		
+		out();
+	}
 }
 echo(`------------ gh-pages updated`);
 out();
